2fdf322963d4a94e4c7c8bcd2e7db275fdb1ff69
Pythomiere <Pythomiere@github.com>
2025-09-16 12:03:03 -0400

    fixed datatypes.java bugs

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,6 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.Assertions.asserEquals;
+
 public class DataTypes {
     public static long sum(int[] numbers) {
-        long s = 0; // variable to accumulate the sum in!
+        int s = 0; // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
